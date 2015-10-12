5b97e0a658e49424778b8ba537a0ff1f6d14f6a8
Oliver Schneider <oliver@assarbad.net>
2015-10-12 20:57:22 +0000

    July version

--- sign-msklc-output.cmd
+++ sign-msklc-output.cmd
@@ -3,7 +3,7 @@
 setlocal ENABLEEXTENSIONS
 set DIR=%~1
 if "%DIR%" == "" echo You must give the path to the MSKLC output & exit /b 1
-::if exist "%~dp0\clean-msklc-output.cmd" call "%~dp0\clean-msklc-output.cmd" "%DIR%"
+if exist "%~dp0\clean-msklc-output.cmd" call "%~dp0\clean-msklc-output.cmd" "%DIR%"
 for /f %%i in ('dir /a-d /b /s "%DIR%"') do @(
   echo Signing %%i
   call ollisign "%%i" "https://assarbad.net" "%~1"
