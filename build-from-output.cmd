1c62a41054ac74623e9a6a754b66ac1e71593ac9
Oliver Schneider <oliver@assarbad.net>
2017-02-13 23:25:55 +0000

    Updating the keyboard layouts, offering a SHA2-signed and a classically signed version of the installer as well.

--- build-from-output.cmd
+++ build-from-output.cmd
@@ -5,5 +5,15 @@ for %%i in (kbdru_us kbdus_xx) do @(
     call "%~dp0sign-msklc-output.cmd" %%i
     "%MAKENSIS%" "%%i.nsi"
     call ollisign.cmd "%%i.exe" "https://assarbad.net/en/stuff/MSKLC/" "%%i"
+    if exist "%%i.sha2" rd /q /s "%%i.sha2"
+    xcopy /e /i /y "%%i" "%%i.sha2"
+    copy /y "%%i.nsi" "%%i.sha2.nsi"
+)
+for %%i in (kbdru_us.sha2 kbdus_xx.sha2) do @(
+    call "%~dp0sign-msklc-output.cmd" /a %%i
+    "%MAKENSIS%" /DBASENAME=%%i "%%i.nsi"
+    call ollisign.cmd /a "%%i.exe" "https://assarbad.net/en/stuff/MSKLC/" "%%i"
+    if exist "%%i" rd /q /s "%%i"
+    if exist "%%i.nsi" del /f "%%i.nsi"
 )
 endlocal
--- sign-msklc-output.cmd
+++ sign-msklc-output.cmd
@@ -1,11 +1,14 @@
 @echo off
 :: Calls ollisign.cmd to sign all files in the given folder
 setlocal ENABLEEXTENSIONS
+if "%~1" == "-a"        shift&set APPENDSHA2=/a
+if "%~1" == "--append"  shift&set APPENDSHA2=/a
+if "%~1" == "/a"        shift&set APPENDSHA2=/a
 set DIR=%~1
 if "%DIR%" == "" echo You must give the path to the MSKLC output & exit /b 1
 if exist "%~dp0\clean-msklc-output.cmd" call "%~dp0\clean-msklc-output.cmd" "%DIR%"
 for /f %%i in ('dir /a-d /b /s "%DIR%"') do @(
   echo Signing %%i
-  call ollisign "%%i" "https://assarbad.net" "%~1"
+  call ollisign %APPENDSHA2% "%%i" "https://assarbad.net" "%~1"
 )
 endlocal
